51858dbdb72de5b72346df4fb979f8cada0b9058
joey gibson <joe.vgibson@gmail.com>
2015-06-25 13:49:01 -0600

    Enter, edit contacts and menu done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,7 @@
 class Contact < ActiveRecord::Base
   validates :Name, presence: true,
-                      length: { minimum: 5 }
-  validates :Email, presence: true,
-                      length: { minimum: 5 }
+                      length: { minimum: 2 }
+  validates :Email, presence: true
   validates :Phone, presence: true,
                       length: { minimum: 5 }
   validates :Address, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Contacts::Application.routes.draw do
   resources :contacts
   root :to => 'welcome#index'
+  match '/home' => 'welcome#index'
+  match '/menu' => 'contacts#menu'
 end
