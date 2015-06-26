fcc9fe00f5128875c3979f31a9be75ce256893c1
joey gibson <joe.vgibson@gmail.com>
2015-06-25 19:41:29 -0600

    front page done. Still have some clean up on other pages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   validates :Name, presence: true,
-                      length: { minimum: 2 }
+                      length: { minimum: 1 }
   validates :Email, presence: true
   validates :Phone, presence: true,
                       length: { minimum: 5 }
