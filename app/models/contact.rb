8d7ad36c9f6e18eb5df73a67438740cd0a2f7476
joey gibson <joe.vgibson@gmail.com>
2015-06-25 23:02:45 -0600

    Final touches on looks and validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,8 @@
 class Contact < ActiveRecord::Base
-  validates :Name, presence: true,
-                      length: { minimum: 1 }
-  validates :Email, presence: true
-  validates :Phone, presence: true,
-                      length: { minimum: 5 }
-  validates :Address, presence: true,
-                      length: { minimum: 5 }
+  validates :Name, presence: true, length: { minimum: 1 }
+  validates :Email, presence: true,
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create,
+                    message: "is not real!" }
+  validates :Phone, presence: true, format: { with: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i, on: :create}
   attr_accessible :Address, :Email, :Name, :Phone
 end
