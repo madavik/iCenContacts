8fcae6a7949864b51b743a47fb9da5a4e5f663a3
joey gibson <joe.vgibson@gmail.com>
2015-07-01 11:10:32 -0600

    made a copy of landing page outside scope of welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Contacts::Application.routes.draw do
   resources :contacts
   root :to => 'welcome#index'
+  get 'welcome/index'
   match '/home' => 'welcome#index'
   match '/menu' => 'contacts#menu'
 end
